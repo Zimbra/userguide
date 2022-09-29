cd24ae742487e9644cdc18cff5010f6b7dae8fbd
Shardool Gore <46771565+shardool-gore@users.noreply.github.com>
2022-09-29 18:47:59 +0530

    Added known issue

--- Zimbra-Desktop/build.sh
+++ Zimbra-Desktop/build.sh
@@ -2,3 +2,6 @@
 
 asciidoctor-pdf -o Zimbra-Desktop.pdf -a linkcss -a z9 -a stylesheet=../stylesheets/asciidoctor-custom.css index.adoc
 echo "Created PDF"
+asciidoctor-pdf -o Zimbra-Desktop.pdf -a linkcss -a z9 -a stylesheet=../stylesheets/asciidoctor-custom.css index.adoc
+echo "Created HTML"
+asciidoctor -b html5 -o zimbra-desktop.html -a linkcss -a z9 -a stylesheet=../stylesheets/asciidoctor-custom.css index.adoc
